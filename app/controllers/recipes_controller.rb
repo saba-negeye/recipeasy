5ffa09a7dbcc8a86e131cb9d934ccf02ebe6e8a9
Saba Negeye <saban@Sabas-Air.hsd1.mn.comcast.net>
2024-12-08 23:56:49 -0600

    json is now parsing correct data, updated recipe form and controller to allow api save. added crsf token, needs authorization

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,6 +4,7 @@ class RecipesController < ApplicationController
 
   def index
     @recipes = current_user.recipes
+    
   end
 
   def show
@@ -14,32 +15,29 @@ class RecipesController < ApplicationController
   end
 
   def create
-    # Check if recipe data is coming from the form submission or the API
-    if params[:recipe][:api_recipe_id].present?
-      # Handle API recipe
-      recipe_data = fetch_api_recipe(params[:recipe][:api_recipe_id])
-      @recipe = current_user.recipes.build(
-        title: recipe_data[:title],
-        description: recipe_data[:description],
-        cooking_steps: recipe_data[:cooking_steps]
-      )
+    @recipe = current_user.recipes.build(recipe_params)
 
-      # Save ingredients from the API
-      recipe_data[:ingredients].each do |ingredient_data|
+    # Additional handling for API data if not covered by recipe_params
+    if request.format.json? && params[:ingredients]
+      params[:ingredients].each do |ingredient_data|
         ingredient = Ingredient.find_or_create_by(name: ingredient_data[:name])
-        RecipeIngredient.create(recipe: @recipe, ingredient: ingredient, quantity: ingredient_data[:quantity])
+        @recipe.recipe_ingredients.build(ingredient: ingredient, quantity: ingredient_data[:quantity])
       end
-    else
-      # Handle form-submitted recipe
-      @recipe = current_user.recipes.build(recipe_params)
     end
 
     if @recipe.save
-      redirect_to recipes_path, notice: 'Recipe was successfully created.'
+      respond_to do |format|
+        format.html { redirect_to recipes_path, notice: 'Recipe was successfully created.' }
+        format.json { render json: @recipe, status: :created }
+      end
     else
-      render :new
+      respond_to do |format|
+        format.html { render :new }
+        format.json { render json: @recipe.errors, status: :unprocessable_entity }
+      end
     end
   end
+
 
   def edit
   end
@@ -59,6 +57,7 @@ class RecipesController < ApplicationController
 
   private
 
+  # Ensure the @recipe instance variable is set for the required actions
   def set_recipe
     @recipe = current_user.recipes.find_by(id: params[:id])
     unless @recipe
@@ -66,25 +65,8 @@ class RecipesController < ApplicationController
     end
   end
 
+  # Strong parameters to allow only specific attributes
   def recipe_params
-    params.require(:recipe).permit(:title, :description, :cooking_steps, recipe_ingredients_attributes: [:ingredient_id, :quantity])
-  end
-
-  # Method to fetch the API recipe data
-  def fetch_api_recipe(api_recipe_id)
-    api_key = "YOUR_SPOONACULAR_API_KEY"
-    url = "https://api.spoonacular.com/recipes/#{api_recipe_id}/information?apiKey=#{api_key}"
-
-    response = Net::HTTP.get(URI(url))
-    recipe_data = JSON.parse(response)
-
-    {
-      title: recipe_data["title"],
-      description: recipe_data["summary"], # Assuming 'summary' is the description
-      cooking_steps: recipe_data["instructions"],
-      ingredients: recipe_data["extendedIngredients"].map do |ingredient|
-        { name: ingredient["name"], quantity: ingredient["amount"], unit: ingredient["unit"] }
-      end
-    }
+    params.require(:recipe).permit(:title, :description, recipe_ingredients_attributes: [:ingredient_id, :quantity])
   end
 end
--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -2,6 +2,5 @@ class RecipeIngredient < ApplicationRecord
   belongs_to :recipe
   belongs_to :ingredient
 
-  validates :quantity, presence: true
-  validates :ingredient_id, presence: true
+ 
 end
